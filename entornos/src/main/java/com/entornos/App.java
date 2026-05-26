c02efe29d637311e3ce25ef2a9e547d369e8fc2c
jemcj <josueskywalker07@gmail.com>
2026-05-26 10:35:16 +0200

    modified:   entornos/src/main/java/com/entornos/App.java 	modified:   entornos/target/classes/com/entornos/App.class

--- entornos/src/main/java/com/entornos/App.java
+++ entornos/src/main/java/com/entornos/App.java
@@ -5,31 +5,31 @@ public class App {
     public static void main(String[] args) {
         //--------------------------------------------------------------------------------------------------------------------------------------------------------
         System.out.println("- Cliente:");
-        cliente c1 = new cliente("Fernando Alonso", "elnano@gmail,com", "Calle Desengaño, 33");
+        Cliente c1 = new Cliente("Fernando Alonso", "elnano@gmail,com", "Calle Desengaño, 33");
         System.out.println("-------------------------------------");
         System.out.println(c1);
         System.out.println("-------------------------------------" + "\n");
         //--------------------------------------------------------------------------------------------------------------------------------------------------------
         System.out.println("- Cesta de la compra:");
-        productoFisico pr1 = new productoFisico("1993XJ220", "F1: Anatomia de una pasion", "Victor Abad", 19.95, 239, 4, "98JS9684DH4");
+        ProductoFisico pr1 = new ProductoFisico("1993XJ220", "F1: Anatomia de una pasion", "Victor Abad", 19.95, 239, 4, "98JS9684DH4");
         System.out.println("Producto 1");
         System.out.println("-------------------------------------");
         System.out.println(pr1);
         System.out.println("-------------------------------------" + "\n");
 
-        productoDigital pr2 = new productoDigital("2010RB600", "Como hacer un coche", "Adrian Newey", 27.50, 6, "PDF");
+        ProductoDigital pr2 = new ProductoDigital("2010RB600", "Como hacer un coche", "Adrian Newey", 27.50, 6, "PDF");
         System.out.println("Producto 2");
         System.out.println("-------------------------------------");
         System.out.println(pr2);
         System.out.println("-------------------------------------" + "\n");
 
-        productoDigital pr3 = new productoDigital("1988MP404", "Los Juegos del Hambre", "Suzanne Collins", 12.30, 4, "PDF");
+        ProductoDigital pr3 = new ProductoDigital("1988MP404", "Los Juegos del Hambre", "Suzanne Collins", 12.30, 4, "PDF");
         System.out.println("Producto 3");
         System.out.println("-------------------------------------");
         System.out.println(pr3);
         System.out.println("-------------------------------------" + "\n");
         //--------------------------------------------------------------------------------------------------------------------------------------------------------
-        pedido pe1 = new pedido("F01I91U10M88", c1);
+        Pedido pe1 = new Pedido("F01I91U10M88", c1);
         pe1.agregarProducto(pr1);
         pe1.agregarProducto(pr2);
         pe1.agregarProducto(pr3);
