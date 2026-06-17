15c5d4f9830de0f786061269daaf9317118202de
jemcj <josueskywalker07@gmail.com>
2026-06-17 19:57:17 +0200

    Creacion de Factura y Tienda (Tiendo no completo)

---
+++ entornos/src/main/java/com/entornos/Factura.java
@@ -0,0 +1,69 @@
+package com.entornos;
+
+import java.time.LocalDate;
+
+public class Factura {
+
+    private int codigo_factura;
+    private LocalDate fecha_emision;
+    private double total_neto;
+    private double total_IVA;
+    private double total_envio;
+    private double total_final;
+
+    public Factura (int codigo_factura, LocalDate fecha_emision, double total_neto, double total_IVA, double total_envio, double total_final) {
+        this.codigo_factura = codigo_factura;
+        this.fecha_emision = LocalDate.now();
+        this.total_neto = total_neto;
+        this.total_IVA = total_IVA;
+        this.total_envio = total_envio;
+        this.total_final = total_final;
+    }
+
+    public int getCodigo_factura() {
+        return codigo_factura;
+    }
+
+    public void setCodigo_factura(int codigo_factura) {
+        this.codigo_factura = codigo_factura;
+    }
+
+    public LocalDate getFecha_emision() {
+        return fecha_emision;
+    }
+
+    //No hay setFecha_emision porque no tiene sentido añadirla, si se genera automaticamente con la fecha de hoy, no hay porque cambiarla a mano mas tarde.
+
+    public double getTotal_neto() {
+        return total_neto;
+    }
+
+    public void setTotal_neto(double total_neto) {
+        this.total_neto = total_neto;
+    }
+
+    public double getTotal_IVA() {
+        return total_IVA;
+    }
+
+    public void setTotal_IVA(double total_IVA) {
+        this.total_IVA = total_IVA;
+    }
+
+    public double getTotal_envio() {
+        return total_envio;
+    }
+
+    public void setTotal_envio(double total_envio) {
+        this.total_envio = total_envio;
+    }
+
+    public double getTotal_final() {
+        return total_final;
+    }
+
+    public void setTotal_final(double total_final) {
+        this.total_final = total_final;
+    }
+    
+}
---
+++ entornos/src/main/java/com/entornos/Tienda.java
@@ -0,0 +1,31 @@
+package com.entornos;
+
+public class Tienda {
+
+    private Cliente cliente;
+    private Pedido pedido;
+
+    public Tienda (Cliente cliente, Pedido pedido) {
+        this.cliente = cliente;
+        this.pedido = pedido;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public 
+    
+}
