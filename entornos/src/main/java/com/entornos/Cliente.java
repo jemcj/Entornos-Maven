acd1325d807d186922a7756c8311d31d1a3b57b0
jemcj <josueskywalker07@gmail.com>
2026-06-17 19:16:26 +0200

    cambios cliente

--- entornos/src/main/java/com/entornos/Cliente.java
+++ entornos/src/main/java/com/entornos/Cliente.java
@@ -2,17 +2,33 @@ package com.entornos;
 
 public class Cliente {
     
+    private String id;
     private String nombre;
     private String correo;
+    private String pais;
     private String direccion;
+    private int agnos_antiguedad;
+    private boolean esVip;
 
-    public Cliente (String nombre, String correo, String direccion){
+    public Cliente (String id, String nombre, String correo, String pais, String direccion, int agnos_antiguedad, boolean esVip){
         if (direccion == null || direccion.isEmpty()) {
             throw new IllegalArgumentException("La dirección no puede estar vacía");
         }
+        this.id = id;
         this.nombre = nombre;
         this.correo = correo;
+        this.pais = pais;
         this.direccion = direccion;
+        this.agnos_antiguedad = agnos_antiguedad;
+        this.esVip = esVip;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getNombre() {
@@ -31,6 +47,14 @@ public class Cliente {
         this.correo = correo;
     }
 
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
     public String getDireccion() {
         return direccion;
     }
@@ -39,11 +63,31 @@ public class Cliente {
         this.direccion = direccion;
     }
 
+    public int getAgnos_antiguedad() {
+        return agnos_antiguedad;
+    }
+
+    public void setAgnos_antiguedad(int agnos_antiguedad) {
+        this.agnos_antiguedad = agnos_antiguedad;
+    }
+
+    public boolean isEsVip() {
+        return esVip;
+    }
+
+    public void setEsVip(boolean esVip) {
+        this.esVip = esVip;
+    }
+
     @Override
     public String toString(){
-        String salida = "Nombre: " + this.nombre + "\n";
+        String salida = "ID: " + this.id + "\n";
+        salida += "Nombre: " + this.nombre + "\n";
         salida += "Correo: " + this.correo + "\n";
+        salida += "Pais: " + this.pais + "\n";
         salida += "Direccion: " + this.direccion + "\n";
+        salida += "Años de antiguedad: " + this.agnos_antiguedad + "\n";
+        salida += "Es VIP: " + this.esVip + "\n";
 
         return salida;
     }
