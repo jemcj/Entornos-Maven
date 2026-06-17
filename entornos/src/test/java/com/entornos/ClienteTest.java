6857721077bf37017f47df089e1e6f9384843d49
jemcj <josueskywalker07@gmail.com>
2026-06-17 23:08:26 +0200

    quitar // de test preparacion para hacerlos

--- entornos/src/test/java/com/entornos/ClienteTest.java
+++ entornos/src/test/java/com/entornos/ClienteTest.java
@@ -8,23 +8,23 @@ import org.junit.jupiter.api.Test;
 
 public class ClienteTest {
 
-    //@Test
-    //@DisplayName("T-05: Los datos del cliente se guardan correctamente")
-    //void T05_cliente_constructorGuardaDatos() {
-    //    
-    //    Cliente cliente = new Cliente("Fernando Alonso", "fnando@test.com", "Calle Mayor 1");
-    //    
-    //    assertEquals("Fernando Alonso", cliente.getNombre());
-    //    assertEquals("fnando@test.com", cliente.getCorreo());
-    //    assertEquals("Calle Mayor 1",   cliente.getDireccion());
-    //}
-//
-    //@Test
-    //@DisplayName("T-08: Lanza excepción si la dirección del cliente está vacía")
-    //void T08_cliente_direccionVacia_lanzaExcepcion() {
-    //    assertThrows(IllegalArgumentException.class, () -> {
-    //        new Cliente("Juan", "juan@test.com", "");
-    //    });
-    //}
+    @Test
+    @DisplayName("T-05: Los datos del cliente se guardan correctamente")
+    void T05_cliente_constructorGuardaDatos() {
+        
+        Cliente cliente = new Cliente("Fernando Alonso", "fnando@test.com", "Calle Mayor 1");
+        
+        assertEquals("Fernando Alonso", cliente.getNombre());
+        assertEquals("fnando@test.com", cliente.getCorreo());
+        assertEquals("Calle Mayor 1",   cliente.getDireccion());
+    }
+
+    @Test
+    @DisplayName("T-08: Lanza excepción si la dirección del cliente está vacía")
+    void T08_cliente_direccionVacia_lanzaExcepcion() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Cliente("Juan", "juan@test.com", "");
+        });
+    }
     
 }
