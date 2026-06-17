3af1f2381421ced1bffe03d3f2eb1d7f3b3daf3c
jemcj <josueskywalker07@gmail.com>
2026-06-17 22:37:48 +0200

    Cambio tonto en producto digital

--- entornos/src/main/java/com/entornos/App.java
+++ entornos/src/main/java/com/entornos/App.java
@@ -3,19 +3,22 @@ package com.entornos;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("- Cliente:");
-        Cliente c1 = new Cliente("Fernando Alonso", "elnano@gmail,com", "Calle Desengaño, 33");
-        ProductoFisico pr1 = new ProductoFisico("1993XJ220", "F1: Anatomia de una pasion", "Victor Abad", 19.95, 239, 4, "98JS9684DH4");
-        ProductoDigital pr2 = new ProductoDigital("2010RB600", "Como hacer un coche", "Adrian Newey", 27.50, 6, "PDF");
-        ProductoDigital pr3 = new ProductoDigital("1988MP404", "Los Juegos del Hambre", "Suzanne Collins", 12.30, 4, "PDF");
+        
+        Cliente c1 = new Cliente("ID0118TYT2126MRMRFMAAM", "Fernando Alonso", "fa14@gmail.com", "España", "Calle Desengaño, 33", 23, true);
+        ProductoFisico pr1 = new ProductoFisico("1993XJ220", "F1: Anatomia de una pasion", "Victor Abad", 19.95, 239, "98JS9684DH4", 1);
+        //ProductoDigital pr2 = new ProductoDigital("2010RB600", "Como hacer un coche", "Adrian Newey", 27.50, 6, "PDF");
+        ProductoDigital pr2 = new ProductoDigital("null", "null", "null", 0, 0, "null");
+        //ProductoDigital pr3 = new ProductoDigital("1988MP404", "Los Juegos del Hambre", "Suzanne Collins", 12.30, 4, "PDF");
 
-        Pedido pe1 = new Pedido("F01I91U10M88", c1);
+
+        //Pedido pe1 = new Pedido("F01I91U10M88", c1);
+
         pe1.agregarProducto(pr1);
         pe1.agregarProducto(pr2);
         pe1.agregarProducto(pr3);
 
-        String resumen = pe1.mostrarResumen();
-        System.out.println(resumen);
+        
+        System.out.println();
 
     }
 
--- entornos/src/main/java/com/entornos/ProductoDigital.java
+++ entornos/src/main/java/com/entornos/ProductoDigital.java
@@ -8,16 +8,12 @@ public class ProductoDigital extends Producto {
     
     private int tamagno_descarga;
     private String tipo_formato;
-    private String tipo_IVA;
 
-    public ProductoDigital (String codigo, String nombre, String autor, double precio, int tamagno_descarga, String tipo_formato, String tipo_IVA){
+
+    public ProductoDigital (String codigo, String nombre, String autor, double precio, int tamagno_descarga, String tipo_formato){
         super(codigo, nombre, autor, precio);
-        if (!tipo_IVA.equalsIgnoreCase("GENERAL") && !tipo_IVA.equalsIgnoreCase("REDUCIDO") && !tipo_IVA.equalsIgnoreCase("SUPER")) {
-            throw new IllegalArgumentException("El tipo de IVA no es válido");
-        }
         this.tamagno_descarga = tamagno_descarga;
         this.tipo_formato = tipo_formato;
-        this.tipo_IVA = tipo_IVA;
     }
 
     public int getTamagno_descarga() {
@@ -34,14 +30,6 @@ public class ProductoDigital extends Producto {
 
     public void setTipo_formato(String tipo_formato) {
         this.tipo_formato = tipo_formato;
-    }
-
-    public String getTipo_IVA() {
-        return tipo_IVA;
-    }
-
-    public void setTipo_IVA(String tipo_IVA) {
-        this.tipo_IVA = tipo_IVA;
     }
     
     @Override
